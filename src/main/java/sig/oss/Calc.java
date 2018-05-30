76c5961af060d6461ee7b783078763d8511a96d7
bluesookyoung <blue@sookmyung.ac.kr>
2018-05-30 15:56:13 +0900

    Update Calc.java

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -1,9 +1,13 @@
 public class Calc{
+	public int r = 0;
 	public int add(int x, int y){ // add method
 		return x + y;
 	}
-	public int sub(int x, int y){
+	public int sub(int x, int y){	// sub method
 		return x - y;
+	}
+	public void dec(int d){	// dec method
+		r -= d;
 	}
 }
 
